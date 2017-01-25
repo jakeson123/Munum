cf5d95801579de6760804d7b030234983f6b3829
Jeisson Sanchez <jeisson_96@hotmail.com>
2017-01-25 00:20:27 -0500

    foudation

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,13 +2,13 @@ var gulp = require('gulp'),
     sass = require('gulp-sass');
 
 gulp.task('sass', function() {
-    return gulp.src('./public/modules/core/scss/*.scss')
+    return gulp.src('./public/modules/core/scss/*.sass')
         .pipe(sass())
         .pipe(gulp.dest('./public/modules/core/css'))
 });
 
 gulp.task('watch', function(){
-  gulp.watch('./public/modules/core/scss/*.scss', ['sass']);
+  gulp.watch('./public/modules/core/scss/*.sass', ['sass']);
 })
 
 gulp.task('default', ['sass', 'watch']);
