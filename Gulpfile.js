1add956d6f35ec4525e705ce9ba8053e9891efea
Jeisson Sanchez <jeisson_96@hotmail.com>
2017-01-26 03:15:06 -0500

    integracion gulp

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,14 +1,44 @@
 var gulp = require('gulp'),
-    sass = require('gulp-sass');
+    sass = require('gulp-sass'),
+    nodemon = require('gulp-nodemon'),
+    browserSync = require('browser-sync'),
+    reload = browserSync.reload;
+
+gulp.task('nodemon', function(cb) {
+    var started = false;
+    return nodemon({
+        script: 'server.js'
+    }).on('start', function() {
+        if (!started) {
+            cb();
+            started = true;
+        }
+    });
+});
+
+gulp.task('browser-sync', ['nodemon'], function() {
+    browserSync.init(["css/*.css", "js/*.js", "html/*.html" ], {
+        proxy: "http://localhost:8080",
+        //files: ["public/modules/core/css/*.*"],
+        port: 7000
+    });
+});
+
+gulp.task('html', function() {
+      return gulp.src('./public/modules/*/views/*.html')
+            .pipe(browserSync.stream());
+    });
 
 gulp.task('sass', function() {
     return gulp.src('./public/modules/core/scss/*.scss')
         .pipe(sass())
         .pipe(gulp.dest('./public/modules/core/css'))
+        .pipe(browserSync.stream());
 });
 
-gulp.task('watch', function(){
-  gulp.watch('./public/modules/core/scss/*.scss', ['sass']);
-})
+gulp.task('watch', function() {
+    gulp.watch('./public/modules/*/scss/*.scss', ['sass']);
+    gulp.watch('./public/modules/*/views/*.html', ['html']);
+});
 
-gulp.task('default', ['sass', 'watch']);
+gulp.task('default', ['sass', 'html',  'browser-sync', 'watch']);
