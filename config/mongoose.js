f5b10037388a907c351497e4ebd799df55f3c24f
Jeisson Sanchez <jeisson_96@hotmail.com>
2017-04-06 23:53:22 -0500

    guardando cambios

--- config/mongoose.js
+++ config/mongoose.js
@@ -7,7 +7,14 @@ module.exports = function() {
 
     //usar Mongoose para conectar a MongoDB
     mongoose.Promise = global.Promise;
-    var db = mongoose.connect(config.db);
+    var db = mongoose.connect(config.db, function (err, res) {
+      if (err) {
+      console.log ('ERROR connecting to');
+      } else {
+      console.log ('Succeeded connected to:');
+      }
+    });
+    //var db = mongoose.createConnection(config.db);
 
     //cargar el modelo'user'
     require('../app/models/user.server.models');
