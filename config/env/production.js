d8ef4b4144285529ca82fca3fa51c4073a5fb784
Jeisson Sanchez <jeisson_96@hotmail.com>
2017-04-06 22:51:34 -0500

    prduccion

--- config/env/production.js
+++ config/env/production.js
@@ -1,27 +1,25 @@
 'use strict';
-
+/*'mongodb://localhost/munum',*/
 module.exports = {
-    db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean',
+    db: 'mongodb://munum:medios2017M@ds155160.mlab.com:55160/munum',
     port: process.env.PORT || 8080,
     sessionSecreta: 'developmentSessionSecret',
     templateEngine: 'pug',
     assets: {
         lib: {
             css: [
-                //'/lib/bootstrap/dist/css/bootstrap.css',
-                //'/modules/core/scss/core.scss'
-                //'/modules/core/css/core.css'
+                '/lib/bootstrap/dist/css/bootstrap.css',
+                '/lib/fonts/fontello/css/fontello.css',
+                '/modules/core/css/core.css',
+                '/modules/core/css/header.css',
+                '/modules/core/css/login.css',
+                '/modules/core/css/social.css'
             ],
             js: [
+                '/lib/jquery/dist/jquery.js',
                 '/lib/angular/angular.min.js',
-                '/lib/bootstrap/dist/js/bootstrap.js',
                 '/lib/angular-resource/angular-resource.min.js',
                 '/lib/angular-ui-router/release/angular-ui-router.min.js',
-
-                '/lib/jquery/dist/jquery.js',
-                //'/lib/what-input/dist/what-input.js',
-                //'/lib/modernizr/modernizr.js',
-                //'/lib/foundation-sites/dist/js/foundation.js',
 
                 '/config.js',
                 '/application.js',
@@ -30,7 +28,8 @@ module.exports = {
                 '/modules/core/core.client.module.js',
                 '/modules/core/config/core.client.routes.js',
                 '/modules/core/controllers/header.client.controller.js',
-                '/modules/core/controllers/home.client.controller.js',
+                '/modules/core/controllers/login.client.controller.js',
+                '/modules/core/controllers/social.client.controller.js',
 
                 //controladores de usuario
                 '/modules/users/users.client.module.js',
@@ -40,6 +39,7 @@ module.exports = {
         }
     }
 };
+
 
 
 
